16aba89a3272e065449d45730c3696f4a95a9d85
Peter Ruprecht <peter.ruprecht@p92rdi.hu>
2017-08-15 15:21:11 +0200

    Added get client token functionality

--- BrainTreeDemo/MainActivity.cs
+++ BrainTreeDemo/MainActivity.cs
@@ -1,17 +1,81 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using Android.Util;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BrainTreeDemo
 {
     [Activity(Label = "BrainTreeDemo", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private static readonly String TAG = typeof(MainActivity).Name;
+        private static readonly String PATH_TO_SERVER = "http://192.168.0.111/braintree/";
+        private static String clientToken;
+        private static readonly int BRAINTREE_REQUEST_CODE = 4949;
+
+        private Button btn_buy;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-             SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            getClientTokenFromServer();
+
+            btn_buy = FindViewById<Button>(Resource.Id.buy_now);
+            btn_buy.Click += delegate
+            {
+                onBraintreeSubmit();
+            };
+        }
+
+        private void getClientTokenFromServer()
+        {
+            Log.Debug(TAG, "getClientTokenFromServer");
+            Task task = new Task(GetClientToken);
+            task.Start();
+        }
+
+        public void onBraintreeSubmit()
+        {
+            //var request = new BTDropInRequest();
+            //var dropIn = new BTDropInController(clientTokenOrTokenizationKey, request, HandleBTDropInControllerHandler);
+            //PresentViewController(dropIn, false, null);
+
+            //DropInRequest dropInRequest = new DropInRequest().clientToken(clientToken);
+            //startActivityForResult(dropInRequest.getIntent(this), BRAINTREE_REQUEST_CODE);
+        }
+
+
+        static async void GetClientToken()
+        {
+            Log.Debug(TAG, "GetClientToken");
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(PATH_TO_SERVER))
+                if (response.IsSuccessStatusCode)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string result = await content.ReadAsStringAsync();
+
+                        if (result != null)
+                        {
+                            clientToken = result;
+                            Log.Debug(TAG, "Client token: " + clientToken);
+                        } else
+                        {
+                            clientToken = "";
+                            Log.Debug(TAG, "Client token is null");
+                        }
+                    }
+                } else
+                {
+                    Log.Debug(TAG, "Unsuccess response: " + response.ToString());
+                }
         }
     }
 }
