3ee53ee0ad20eda02dcb3cc5ac0d02f375aafc43
Peter Ruprecht <peter.ruprecht@p92rdi.hu>
2017-08-15 08:52:55 +0200

    Project created

--- BrainTreeDemo/MainActivity.cs
+++ BrainTreeDemo/MainActivity.cs
@@ -11,8 +11,7 @@ namespace BrainTreeDemo
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+             SetContentView (Resource.Layout.Main);
         }
     }
 }
